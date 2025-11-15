6e6fcec5f17c74608250f8f010062f235eeb56cb
Taha Mehjabin <marvime660@gmail.com>
2025-11-15 06:43:39 +0600

    Add files via upload

---
+++ api/skills_dictionary.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    "php" => ["php", "core php", "backend php", "php framework"],
+    "javascript" => ["javascript", "js", "ecmascript", "frontend js"],
+    "html" => ["html", "html5"],
+    "css" => ["css", "css3"],
+    "mysql" => ["mysql", "database", "sql", "sql database"],
+    "react" => ["react", "reactjs", "react.js", "react framework"],
+    "laravel" => ["laravel", "php laravel framework"],
+    "python" => ["python", "python3", "data analysis", "machine learning"],
+    "java" => ["java", "java programming", "java developer"],
+    "teamwork" => ["teamwork", "collaboration", "group work"],
+    "communication" => ["communication", "public speaking"],
+    "problem solving" => ["problem solving", "critical thinking", "analytical skills"]
+];
